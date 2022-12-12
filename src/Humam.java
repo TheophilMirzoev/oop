d7c0433b1176d35ef45d0fa27ac4f7072d66488a
TheophilMirzoev <115739938+TheophilMirzoev@users.noreply.github.com>
2022-12-12 21:25:43 +0300

    first

--- src/Humam.java
+++ src/Humam.java
@@ -3,7 +3,6 @@ public class Humam {
     String name;
     String town;
     String jobTitle;
-
     public void introduceYourself () {
         System.out.println("Привет! Меня зовут " + name + " Я из города " + town + " родил ся/ась в "
                 + yearOfBirth  +  " году. Я работаю на должности " + jobTitle + "." + " Будем знакомы! ");
