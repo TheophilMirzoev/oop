43dcdf66d7663237fb9eace8951078b627a171b2
TheophilMirzoev <115739938+TheophilMirzoev@users.noreply.github.com>
2022-12-13 21:28:10 +0300

    first

--- src/Humam.java
+++ src/Humam.java
@@ -3,8 +3,38 @@ public class Humam {
     String name;
     String town;
     String jobTitle;
-    public void introduceYourself () {
+
+    public Humam(String name, int yearOfBirth, String town, String jobTitle) {
+        if (this.yearOfBirth > 0) {
+            this.yearOfBirth = yearOfBirth;
+        } else if (this.yearOfBirth < 0) {
+            this.yearOfBirth = 0;
+        }
+
+        if (this.town != null) {
+            this.town = town;
+        } else if (this.town == null) {
+            this.town = "Информация не указана";
+        }
+        if (this.jobTitle != null) {
+            this.jobTitle = jobTitle;
+        } else if (this.jobTitle == null) {
+            this.jobTitle = "Информация не указана";
+        }
+
+        if (this.name != null) {
+            this.name = name;
+        } else if (this.name == null) {
+            this.name = "Информация не указана";
+        }
+
+    }
+    void protectFromNull() {
+
+    }
+
+    public void introduceYourself() {
         System.out.println("Привет! Меня зовут " + name + " Я из города " + town + " родил ся/ась в "
-                + yearOfBirth  +  " году. Я работаю на должности " + jobTitle + "." + " Будем знакомы! ");
+                + yearOfBirth + " году. Я работаю на должности " + jobTitle + "." + " Будем знакомы! ");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        Humam humam1 = new Humam();
-        humam1.name = "Максим";
-        humam1.yearOfBirth = 1988;
-        humam1.town = "Минск";
-        humam1.jobTitle = "бренд-менеджер";
-
-        Humam humam2 = new Humam();
-        humam2.name = "Аня";
-        humam2.yearOfBirth = 1993;
-        humam2.town = "Москва";
-        humam2.jobTitle = "методист образовательных программ";
-
-        Humam humam3 = new Humam();
-        humam3.name = "Катя";
-        humam3.yearOfBirth = 1992;
-        humam3.town = "Калининград";
-        humam3.jobTitle = "продакт-менеджер";
-
-        Humam humam4 = new Humam();
-        humam4.name = "Москва";
-        humam4.yearOfBirth = 1995;
-        humam4.town = "Москва";
-        humam4.jobTitle = "директор по развитию бизнеса";
+        Humam humam1 = new Humam("Максим", 1988, "Минск", "бренд-менеджер");
+        Humam humam2 = new Humam("Аня",1993, "Москва", "методист образовательных программ");
+        Humam humam3 = new Humam("Катя", 1992, "Калининград", "продакт-менеджер");
+        Humam humam4 = new Humam("Москва", 1995, "Москва", "директор по развитию бизнеса");
 
         humam1.introduceYourself();
         humam2.introduceYourself();
